444950f5777257a8808b9ecad4f503a94f43b2c4
vedant-g1 <vedant.golhani@gmail.com>
2020-12-19 19:22:47 +0530

    next

--- src/main/java/com/hackathon/memesapp/MainActivity.java
+++ src/main/java/com/hackathon/memesapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hackathon.memesapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,6 +13,10 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -20,6 +25,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.android.volley.Request;
@@ -87,7 +93,13 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
     private void loadMeme(){
+        final ProgressBar simpleProgressBar=(ProgressBar) findViewById(R.id.progressBar); // initiate the progress bar
+        simpleProgressBar.setVisibility(View.VISIBLE);
+
+
+
         // Instantiate the RequestQueue.
+
         RequestQueue queue = Volley.newRequestQueue(this);
         String url ="https://meme-api.herokuapp.com/gimme";
 
@@ -105,7 +117,35 @@ public class MainActivity extends AppCompatActivity {
                             e.printStackTrace();
                         }
                         ImageView imageView = (ImageView) findViewById(R.id.memeImageView);
-                        Glide.with(getApplicationContext()).load(url).into(imageView);
+                        Glide.with(getApplicationContext())
+                                .load(url)
+                                .listener(new RequestListener<Drawable>() {
+                            @Override
+                            public boolean onLoadFailed(@Nullable GlideException e,
+                                                        Object model,
+                                                        Target<Drawable> target,
+                                                        boolean isFirstResource) {
+
+                                simpleProgressBar.setVisibility(View.GONE);
+                                return false;
+
+
+                            }
+
+
+                                    @Override
+                            public boolean onResourceReady(Drawable resource,
+                                                           Object model,
+                                                           Target<Drawable> target,
+                                                           DataSource dataSource,
+                                                           boolean isFirstResource) {
+
+                                 simpleProgressBar.setVisibility(View.GONE);
+                                 return false;
+
+
+                                    }
+                        }).into(imageView);
                     }
                 }, new Response.ErrorListener() {
 
@@ -122,6 +162,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void showNextMeme(View view) {
+        loadMeme();
     }
 
     public void shareMeme(View view) {
