8ab8d6477d19b6c42ae1248800925c28f6445fc5
vedant-g1 <vedant.golhani@gmail.com>
2020-12-19 14:16:01 +0530

    meme image view

--- src/main/java/com/hackathon/memesapp/MainActivity.java
+++ src/main/java/com/hackathon/memesapp/MainActivity.java
@@ -7,8 +7,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+//import com.bumptech.glide.Glide;
+//import com.bumptech.glide.load.DataSource;
+//import com.bumptech.glide.load.engine.GlideException;
+//import com.bumptech.glide.request.RequestListener;
+//import com.bumptech.glide.request.target.Target;
+//import kotlinx.android.synthetic.main.activity_main.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,10 +46,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//
-//        mAuth = FirebaseAuth.getInstance();
-//        mCurrentUser = mAuth.getCurrentUser();
-//
+
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+
+        loadMeme();
+
+
+
+
+
+
 //        mLogoutBtn = findViewById(R.id.logout_btn);
 //
 //        mLogoutBtn.setOnClickListener(new View.OnClickListener() {
@@ -53,5 +85,45 @@ public class MainActivity extends AppCompatActivity {
         loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(loginIntent);
         finish();
+    }
+    private void loadMeme(){
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://meme-api.herokuapp.com/gimme";
+
+        // Request a string response from the provided URL.
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+//                        textView.setText("Response: " + response.toString());
+                        String url= null;
+                        try {
+                            url = response.getString("url");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        ImageView imageView = (ImageView) findViewById(R.id.memeImageView);
+                        Glide.with(getApplicationContext()).load(url).into(imageView);
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest);
+    }
+
+
+    public void showNextMeme(View view) {
+    }
+
+    public void shareMeme(View view) {
     }
 }
